8b47017c20a624bc83b8032c88a8e3ce88672800
alanshary <zal82062@gmail.com>
2025-01-05 06:27:54 +0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+class Data:
+    """Class untuk menyimpan data nilai UAS."""
+    def __init__(self, nama, nim, nilai_uas):
+        self.nama = nama
+        self.nim = nim
+        self.nilai_uas = nilai_uas
